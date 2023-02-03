be70728882c5e86439f289db2e6eb7abfd72601c
28Pollux28 <lemaire_valentin28@yahoo.fr>
2023-02-03 11:33:16 +0100

    Added help message + output for async programs

--- commands/commands.js
+++ commands/commands.js
@@ -19,7 +19,13 @@ let formats = {
 }
 
 function help(TCCLI, asyncB, args) {
-    return formats.help("help") + " - " + formats.info("Shows this help message");
+    return formats.help("help") + " - " + formats.info("Shows this help message") + os.EOL +
+        formats.help("exit") + " - " + formats.info("Exits the program") + os.EOL +
+        formats.help("cd") + " - " + formats.info("Changes the current directory") + os.EOL +
+        formats.help("start") + " - " + formats.info("Starts a program - Add ! at the end to start in async modehe") + os.EOL +
+        formats.help("lp") + " - " + formats.info("Lists all programs attached to this CLI") + os.EOL +
+        formats.help("bing") + " - " + formats.info("Kills, pauses or continues a process attached to this CLI") + os.EOL +
+        formats.help("keep") + " - " + formats.info("Detaches a program from the CLI") + os.EOL
 }
 
 function exit(TCCLI, asyncB, args) {
@@ -46,13 +52,16 @@ function start(TCCLI, asyncB, args) {
         return which(args[0])
             .catch((e) => args[0])
             .then((path) => {
-                return spawn(path, args.slice(1),{detached: true, stdio: 'ignore'});
+                return spawn(path, args.slice(1),{detached: true});
             })
             .then((child) => {
                 TCCLI.addProcess(child);
                 child.on('error', (e) => {
                     TCCLI.log(formats.error("Cannot start " + args[0] + " in async mode"));
                     TCCLI.removeProcess(child);
+                });
+                child.stdout.on('data', (data) => {
+                    TCCLI.log(formats.info(iconv.decode(data, 'cp437')));
                 });
                 child.on('exit', (code) => {
                     TCCLI.removeProcess(child);
@@ -138,6 +147,12 @@ function keep(TCCLI, asyncB, args) {
     //Detach the process with Pid from the parent
     TCCLI.getProcesses().filter((child) => child.pid === parseInt(args[0])).map((child) =>{
         child.unref();
+        child.stderr.unpipe()
+        child.stderr.destroy()
+        child.stdout.unpipe()
+        child.stdout.destroy()
+        child.stdin.end()
+        child.stdin.destroy()
         TCCLI.removeProcess(child);
     });
     return formats.success("Detached process with PID " + args[0]);
